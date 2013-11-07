3641629715b5073e904635d7b3c0d588ca0c36a9
Matthew (newz) Nuzum <NuzumMatthewE@JohnDeere.com>
2013-11-07 16:58:44 -0600

    Adding initial

---
+++ public/javascripts/main.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready( function() {
+
+	$('.carousel').carousel({
+		interval: false
+	})
+
+});
+
+// To view the next image in the carousel:
+//		$('.carousel').carousel('prev')
+// To view the prev image in the carousel:
+// 		$('.carousel').carousel('next')
+
