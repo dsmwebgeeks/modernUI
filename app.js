9312567cdc6a3657143c2b0be4074c442dc40f7a
Matthew (newz) Nuzum <NuzumMatthewE@JohnDeere.com>
2013-11-08 15:54:43 -0600

    enabling node webserver

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
