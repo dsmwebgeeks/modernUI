e1d951cc0b5735bea2e4458c71d834f2a492c7d5
Justin Stevens <justinstevens@itracedesigns.com>
2013-11-09 23:05:42 -0600

    Added off canvase menu, text changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,34 @@
 
+$(document).ready(function() {
 
-$(document).ready( function() {
-
-	$('.carousel').carousel({
-		interval: false
-	})
+  /* ---------- Acivate Functions ---------- */
+  template_functions();
+  init_hpcarousel();
 
 });
 
-// To view the next image in the carousel:
-//		$('.carousel').carousel('prev')
-// To view the prev image in the carousel:
-// 		$('.carousel').carousel('next')
+/* ---------- Check Mark on Media Items ---------- */
+function init_hpcarousel() {
+    if($('.carousel').length){
+			$('.carousel').carousel({
+				interval: false
+			})
 
+			// To view the next image in the carousel:
+			//		$('.carousel').carousel('prev')
+			// To view the prev image in the carousel:
+			// 		$('.carousel').carousel('next')
+
+		}
+}
+
+/* ---------- Check Mark on Media Items ---------- */
+function template_functions() {
+    if($('.panel-navigation-left').length){
+      $('.trigger-left-panel').on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("offcanvas-out");
+        $(".page-outer").toggleClass("left");
+      });
+    }
+}
