0bdbe6e58da0a13559261cc48ced9456688e11f4
Matthew (newz) Nuzum <NuzumMatthewE@JohnDeere.com>
2013-11-11 13:50:49 -0600

    adding icons to api and enabling refresh button

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,8 +4,46 @@ $(document).ready(function() {
   /* ---------- Acivate Functions ---------- */
   template_functions();
   init_hpcarousel();
-
+  enable_reload();
+  $('#icon-list').html('');
+  reloadIcons();
 });
+
+function showIcons(response) {
+  var html = '';
+  if(response.api == 'ok') {
+    $.each(response.icons, function(index, item) {
+      html += '<div class="col-md-4">';
+      html += '<img class="img-thumbnail" src="';
+      html += item.path;
+      html += '"><h2><a href="';
+      html += item.link;
+      html += '" target="_blank">'
+      html += item.name
+      html += '</a></h2><p>'
+      html += item.attr;
+      html += '</p><p><a class="btn btn-default" href="';
+      html += item.link;
+      html += '" role="button">View details &raquo;</a></p></div>';
+    });
+
+    $('#icon-list').html(html);
+  }
+}
+
+function reloadIcons() {
+  console.log('Reloading!');
+  $.ajax({
+    url: '/api'
+  }).success(showIcons);
+  // $.get('/api', showIcons);
+}
+
+function enable_reload() {
+  console.log('reloading enabled');
+  $('.page-heading i.icon-reload').removeClass('hide').on('click', reloadIcons);
+
+}
 
 /* ---------- Check Mark on Media Items ---------- */
 function init_hpcarousel() {
--- routes/index.js
+++ routes/index.js
@@ -5,40 +5,94 @@
 
 var icons = [
     { 
-        path: 'images/noun_project_24414.png',
-        name: 'International Space Station1',
+        path: 'images/noun_project_12346.png',
+        name: 'Alien Abduction',
         link: '',
-        attr: '<p>Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project</p>'
+        attr: 'Designed by <a href="http://thenounproject.com/jmkeuning" target="_blank">James Keuning </a> from The Noun Project'
     }, 
     { 
-        path: 'images/noun_project_24414.png',
-        name: 'International Space Station2',
+        path: 'images/noun_project_12458.png',
+        name: 'Death Star',
         link: '',
-        attr: '<p>Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project</p>'
+        attr: 'Designed by <a href="http://thenounproject.com/iconoci" target="_blank">iconoci</a> from The Noun Project'
     }, 
     { 
-        path: 'images/noun_project_24414.png',
-        name: 'International Space Station3',
+        path: 'images/noun_project_16286.png',
+        name: 'Astronaut',
         link: '',
-        attr: '<p>Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project</p>'
+        attr: 'Designed by <a href="http://thenounproject.com/avasiloiei.liviu" target="_blank">Avasiloiei Liviu</a> from The Noun Project'
     }, 
     { 
-        path: 'images/noun_project_24414.png',
-        name: 'International Space Station4',
+        path: 'images/noun_project_170.png',
+        name: 'Sun',
         link: '',
-        attr: '<p>Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project</p>'
+        attr: 'Designed by <a href="http://thenounproject.com/noun/sun/#icon-No170" target="_blank">Unknown Designer</a> from The Noun Project'
     }, 
     { 
-        path: 'images/noun_project_24414.png',
-        name: 'International Space Station5',
+        path: 'images/noun_project_17621.png',
+        name: 'Space Invader',
         link: '',
-        attr: '<p>Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project</p>'
+        attr: 'Designed by <a href="http://thenounproject.com/TheGiantBaboon" target="_blank">James Rainsford</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_23971.png',
+        name: 'Hubble Space Telescope',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/nrodriguezlima" target="_blank">nikki rodriguez</a> from The Noun Project'
     }, 
     { 
         path: 'images/noun_project_24414.png',
         name: 'International Space Station6',
         link: '',
-        attr: '<p>Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project</p>'
+        attr: 'Designed by <a href="http://thenounproject.com/johnnyrocketcreative">Jon Daiello</a> 2013 from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_24736.png',
+        name: 'Space Tower',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/fivecity_5c" target="_blank">José Manuel Rodríguez</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_3590.png',
+        name: 'Space Hopper',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/tokefrello" target="_blank">Toke Frello</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_4917.png',
+        name: 'Mars Rover',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/serre.marc" target="_blank">Marc Serre</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_709.png',
+        name: 'Space Shuttle',
+        link: '',
+        attr: 'From The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_7478.png',
+        name: 'Earth',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/nicografico" target="_blank">Nicolas Ramallo</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_7536.png',
+        name: 'Solar System',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/misirlou" target="_blank">Benjamin Orlovski</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_792.png',
+        name: 'Planet',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/tokefrello" target="_blank">Toke Frello</a> from The Noun Project'
+    }, 
+    { 
+        path: 'images/noun_project_8661.png',
+        name: 'Space Needle',
+        link: '',
+        attr: 'Designed by <a href="http://thenounproject.com/kellydh" target="_blank">Kelly Hamilton</a> from The Noun Project'
     }
     ];
 
